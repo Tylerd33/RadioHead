f3ad4bce39ed3302efa8a1e36e5f45577aa1ae76
Tylerd33 <rossityler3@gmail.com>
2024-09-10 20:27:15 -0400

    Added Basic URL storer

---
+++ Scripts/URLStorer.ps1
@@ -0,0 +1,20 @@
+﻿Write-OutPut "Welcome to the URL library, this can be used to store URLs of important videos/audios that you would like to use later"
+$UserInput1 = ""
+$UserInput2 = ""
+if(Test-Path -Path MediaLib.txt){
+    Write-Output "MediaLib exists, using existing MediaLib.txt"
+}
+else{
+    Write-OutPut "MediaLib.txt doesn't exist, creating"
+    New-Item -ItemType "directory" MediaLib
+}
+
+#Write to .txt file loop
+while($UserInput1 -ne "done"){
+    $UserInput1 = Read-Host "Enter URL only or done if finished: "
+    $UserInput2 = Read-Host "Enter Content of URL only"
+    if($UserInput1 -ne "" -and $UserInput1 -ne "done"){
+        $UserInput1 = "$($UserInput1) $($UserInput2)"
+        Write-OutPut $UserInput1 >> MediaLib.txt
+    }
+} 
